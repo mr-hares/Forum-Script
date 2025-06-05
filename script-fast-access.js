922d426b18e1a07636ea63de221b72ef6f66a3fe
Luis-Moretti <ma4muh@gmail.com>
2025-06-05 13:35:58 +0300

    Update script-fast-access.js

--- script-fast-access.js
+++ script-fast-access.js
@@ -15,7 +15,7 @@ function createbutton(name, link) {
     });
     bgButtons.append(button);
 }
-createbutton('Админ-раздел', 'https://forum.blackrussia.online/forums/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB.2180/');
+// createbutton('Админ-раздел', 'https://forum.blackrussia.online/forums/%D0%90%D0%B4%D0%BC%D0%B8%D0%BD-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB.2180/');
 createbutton('Норматив ГОСС', 'https://forum.blackrussia.online/threads/khabarovsk-%D0%95%D0%B6%D0%B5%D0%B4%D0%BD%D0%B5%D0%B2%D0%BD%D0%B0%D1%8F-%D0%9E%D1%82%D1%87%D1%91%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D0%9B%D0%B8%D0%B4%D0%B5%D1%80%D0%BE%D0%B2.10962757/page-10000');
 createbutton('Онлайн ГОСС', 'https://forum.blackrussia.online/threads/khabarovsk-%D0%95%D0%B6%D0%B5%D0%B4%D0%BD%D0%B5%D0%B2%D0%BD%D0%B0%D1%8F-%D0%BE%D1%82%D1%87%D1%91%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C-%D0%BB%D0%B8%D0%B4%D0%B5%D1%80%D0%BE%D0%B2-%D0%BE%D0%B1-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD%D0%B5.6065801/page-10000');
 createbutton('Лидерский раздел', 'https://forum.blackrussia.online/forums/%D0%9B%D0%B8%D0%B4%D0%B5%D1%80%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D0%B0%D0%B7%D0%B4%D0%B5%D0%BB.2187/');
